7d3438b8cffa9760308dddc45ac18dca44640489
Joshlanuevo <lanuevo68@gmail.com>
2024-08-17 15:39:52 +0800

    add images, data class, and model

--- RecyclerViewCompose/app/src/main/java/com/vancoding/recyclerviewcompose/MainActivity.kt
+++ RecyclerViewCompose/app/src/main/java/com/vancoding/recyclerviewcompose/MainActivity.kt
@@ -7,21 +7,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.vancoding.recyclerviewcompose.ui.theme.RecyclerViewComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
