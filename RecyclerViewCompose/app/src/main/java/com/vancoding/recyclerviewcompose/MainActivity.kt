0b0c4af436980a91ec3f5f4f46208c4f08c773b8
Joshlanuevo <lanuevo68@gmail.com>
2024-08-17 16:24:56 +0800

    add functionalities

--- RecyclerViewCompose/app/src/main/java/com/vancoding/recyclerviewcompose/MainActivity.kt
+++ RecyclerViewCompose/app/src/main/java/com/vancoding/recyclerviewcompose/MainActivity.kt
@@ -5,19 +5,40 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.example.composerecyclerview.data.TvShowList
+import com.example.composerecyclerview.model.TvShow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +47,18 @@ class MainActivity : ComponentActivity() {
         setContent {
 //            ScrollableColumnDemo()
 //            LazyColumnDemo()
-            LazyColumnDemo2 {
-                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+//            LazyColumnDemo2 {
+//                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+//            }
+            DisplayTvShows {
+                Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
             }
         }
     }
 }
 
 @Composable
-fun ScrollableColumnDemo() {
+fun ScrollableColumnDemo()  {
     val scrollState = rememberScrollState()
     Column (
         modifier = Modifier.verticalScroll(scrollState)
@@ -78,4 +102,78 @@ fun LazyColumnDemo2(selectedItem: (String)->(Unit)) {
             Divider(color = Color.Black, thickness = 1.dp)
         }
     }
+}
+
+@Composable
+fun DisplayTvShows(selectedItem: (TvShow) -> Unit) {
+    val tvShows = remember { TvShowList.tvShows }
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(
+            items = tvShows,
+            itemContent = {
+                TvShowListItem(tvShow = it, selectedItem)
+            }
+        )
+    }
+}
+
+@Composable
+fun TvShowListItem(tvShow: TvShow, selectedItem: (TvShow) -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp) ,
+        shape = RoundedCornerShape(corner = CornerSize(10.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth()
+                .clickable { selectedItem(tvShow) },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TvShowImage(tvShow = tvShow)
+            Column {
+                Text(
+                    text = tvShow.name,
+                    style = MaterialTheme.typography.titleSmall
+                )
+                Spacer(
+                    modifier = Modifier.height(4.dp)
+                )
+                Text(
+                    text = tvShow.overview,
+                    style = MaterialTheme.typography.bodyLarge,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(
+                    modifier = Modifier.height(8.dp)
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = tvShow.year.toString(), style = MaterialTheme.typography.headlineSmall)
+                    Text(text = tvShow.rating.toString(), style = MaterialTheme.typography.headlineSmall)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TvShowImage(tvShow: TvShow) {
+    Image(
+        painter = painterResource(id = tvShow.imageId),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .padding(4.dp)
+            .height(140.dp)
+            .width(100.dp)
+            .clip(RoundedCornerShape(corner = CornerSize(10.dp))))
 }
