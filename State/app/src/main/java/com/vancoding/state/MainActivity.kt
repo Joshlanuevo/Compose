57692672b83b57a57fa9af5aa0077dae10f6c748
Joshlanuevo <lanuevo68@gmail.com>
2024-08-19 14:10:12 +0800

    add state

--- State/app/src/main/java/com/vancoding/state/MainActivity.kt
+++ State/app/src/main/java/com/vancoding/state/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.vancoding.state
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.vancoding.state.ui.theme.StateTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +35,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StateTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    MyButton()
                 }
             }
         }
     }
 }
 
+@Preview(name = "MyButton")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyButton() {
+    var count by remember { mutableStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StateTheme {
-        Greeting("Android")
+    Button(
+        onClick = {
+            count += 1
+        },
+        modifier = Modifier
+            .padding(16.dp)
+            .border(BorderStroke(2.dp, Color.Black)),
+        contentPadding = PaddingValues(16.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.DarkGray,
+            contentColor = Color.White
+        )
+    ) {
+        Text(
+            text = "Count is: $count",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(5.dp)
+        )
     }
 }
