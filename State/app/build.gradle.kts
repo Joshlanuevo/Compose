c63d3648df61dd1f678d65c828391067c70e3f98
Joshlanuevo <lanuevo68@gmail.com>
2024-08-19 14:38:47 +0800

    apply viewmodel

--- State/app/build.gradle.kts
+++ State/app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Optional - Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 }
--- State/app/src/main/java/com/vancoding/state/MainActivity.kt
+++ State/app/src/main/java/com/vancoding/state/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +28,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vancoding.state.ui.theme.StateTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,14 +36,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var viewmodel = viewModel<MyViewModel>()
             StateTheme {
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    var count by remember { mutableStateOf(0) }
-                    MyButton(count) { count = it + 1 }
+                    val count = viewmodel.count
+                    MyButton(currentCount = count) {
+                        viewmodel.increaseCount()
+                    }
                 }
             }
         }
