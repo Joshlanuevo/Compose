070fb3bfd550ba2528a16ed006aad9cbbf5f7d68
Joshlanuevo <lanuevo68@gmail.com>
2024-09-03 10:01:26 +0800

    add new project

--- State/app/src/main/java/com/vancoding/state/MyViewModel.kt
+++ State/app/src/main/java/com/vancoding/state/MyViewModel.kt
@@ -1,12 +1,13 @@
 package com.vancoding.state
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
-    var count by mutableStateOf(0)
+    var count by mutableIntStateOf(0)
 
     fun increaseCount() {
         count++
