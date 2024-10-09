78c5e9caa20fe67599f43f053e16773c60b4f5ef
Joshlanuevo <lanuevo68@gmail.com>
2024-10-09 21:30:05 +0800

    Add Home Screen

---
+++ NavigationCompose/app/src/main/java/com/vancoding/navigationcompose/HomeScreen.kt
@@ -0,0 +1,28 @@
+package com.vancoding.navigationcompose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Text("Home Screen")
+            Button(
+                onClick = { navController.navigate("second_screen") }
+            ) {
+                Text("Go to Second Screen")
+            }
+        }
+    }
+}
