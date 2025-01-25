e52f0fe414da12ff4794abcabe9ba5afc0af633f
Joshlanuevo <lanuevo68@gmail.com>
2025-01-25 15:12:39 +0800

    setup nav host controller

--- Todo/app/src/main/java/com/vancoding/todo/navigation/Screens.kt
+++ Todo/app/src/main/java/com/vancoding/todo/navigation/Screens.kt
@@ -1,4 +1,16 @@
 package com.vancoding.todo.navigation
 
-class Screens {
+import androidx.navigation.NavHostController
+import com.vancoding.todo.utils.Action
+import com.vancoding.todo.utils.Constants.LIST_SCREEN
+
+class Screens(navHostController: NavHostController) {
+    val list: (Action) -> Unit = { action ->
+        navHostController.navigate("list/${action.name}") {
+            popUpTo(LIST_SCREEN) { inclusive = true }
+        }
+    }
+    val task: (Int) -> Unit = { taskId ->
+        navHostController.navigate("task/$taskId")
+    }
 }
