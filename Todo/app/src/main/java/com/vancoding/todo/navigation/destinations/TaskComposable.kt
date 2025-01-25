b75f42c7305255c1628da4465331d2b39602d2e9
Joshlanuevo <lanuevo68@gmail.com>
2025-01-25 15:33:34 +0800

    setup task composable navigation

---
+++ Todo/app/src/main/java/com/vancoding/todo/navigation/destinations/TaskComposable.kt
@@ -0,0 +1,22 @@
+package com.vancoding.todo.navigation.destinations
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.vancoding.todo.utils.Action
+import com.vancoding.todo.utils.Constants.TASK_ARGUMENT_KEY
+import com.vancoding.todo.utils.Constants.TASK_SCREEN
+
+fun NavGraphBuilder.taskComposable(
+    navigateToListScreen: (Action) -> Unit,
+) {
+    composable(
+        route = TASK_SCREEN,
+        arguments = listOf(navArgument(TASK_ARGUMENT_KEY) {
+            type = NavType.IntType
+        })
+    ) {
+
+    }
+}
