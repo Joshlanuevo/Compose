55b358be9e2f378286275fef8206184153bf28ba
Joshlanuevo <lanuevo68@gmail.com>
2025-01-29 17:49:28 +0800

    add existing task app bar

--- Todo/app/src/main/java/com/vancoding/todo/ui/screens/task/TaskAppBar.kt
+++ Todo/app/src/main/java/com/vancoding/todo/ui/screens/task/TaskAppBar.kt
@@ -3,6 +3,8 @@ package com.vancoding.todo.ui.screens.task
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
@@ -12,8 +14,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.vancoding.todo.R
+import com.vancoding.todo.data.models.Priority
+import com.vancoding.todo.data.models.ToDoTask
 import com.vancoding.todo.ui.theme.topAppBarBackgroundColor
 import com.vancoding.todo.ui.theme.topAppBarContentColor
 import com.vancoding.todo.utils.Action
@@ -23,6 +28,12 @@ fun TaskAppBar(
     navigateToListScreen: (Action) -> Unit,
 ) {
     NewTaskAppBar(navigateToListScreen = navigateToListScreen)
+    ExistingTaskAppBar(selectedTask = ToDoTask(
+        id = 0,
+        title = "Task Title",
+        description = "Task Description",
+        priority = Priority.LOW
+    ), navigateToListScreen = navigateToListScreen)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -79,8 +90,95 @@ fun AddAction(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExistingTaskAppBar(
+    selectedTask: ToDoTask,
+    navigateToListScreen: (Action) -> Unit,
+) {
+    TopAppBar(
+        navigationIcon = {
+            CloseAction(onCloseClicked = navigateToListScreen)
+        },
+        title = {
+            Text(
+                text = selectedTask.title,
+                color = MaterialTheme.colorScheme.topAppBarContentColor,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.topAppBarBackgroundColor,
+        ),
+        actions = {
+            DeleteAction(onDeleteClicked = navigateToListScreen)
+            UpdateAction(onUpdateClicked = navigateToListScreen)
+        }
+    )
+}
+
+@Composable
+fun CloseAction(
+    onCloseClicked: (Action) -> Unit,
+) {
+    IconButton(
+        onClick = { onCloseClicked(Action.NO_ACTION) }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Close,
+            contentDescription = stringResource(id = R.string.close_icon),
+            tint = MaterialTheme.colorScheme.topAppBarContentColor,
+        )
+    }
+}
+
+@Composable
+fun DeleteAction(
+    onDeleteClicked: (Action) -> Unit,
+) {
+    IconButton(
+        onClick = { onDeleteClicked(Action.DELETE) }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Delete,
+            contentDescription = stringResource(id = R.string.close_icon),
+            tint = MaterialTheme.colorScheme.topAppBarContentColor,
+        )
+    }
+}
+
+@Composable
+fun UpdateAction(
+    onUpdateClicked: (Action) -> Unit,
+) {
+    IconButton(
+        onClick = { onUpdateClicked(Action.UPDATE) }
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Check,
+            contentDescription = stringResource(id = R.string.update_icon),
+            tint = MaterialTheme.colorScheme.topAppBarContentColor,
+        )
+    }
+}
+
 @Composable
 @Preview
 fun NewTaskAppBarPreview() {
     NewTaskAppBar(navigateToListScreen = {})
+}
+
+@Composable
+@Preview
+fun ExistingTaskAppBarPreview() {
+    ExistingTaskAppBar(
+        selectedTask = ToDoTask(
+            id = 0,
+            title = "Task Title",
+            description = "Task Description",
+            priority = Priority.LOW
+        ),
+        navigateToListScreen = {}
+    )
 }
