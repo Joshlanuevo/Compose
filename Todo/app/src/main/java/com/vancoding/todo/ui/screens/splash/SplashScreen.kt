1aa1e5d9c1212ed0bd92ab4f7447d59f414e9633
Joshlanuevo <lanuevo68@gmail.com>
2025-02-03 16:05:14 +0800

    design splash screen

--- Todo/app/src/main/java/com/vancoding/todo/ui/screens/splash/SplashScreen.kt
+++ Todo/app/src/main/java/com/vancoding/todo/ui/screens/splash/SplashScreen.kt
@@ -1,2 +1,51 @@
 package com.vancoding.todo.ui.screens.splash
 
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.vancoding.todo.R
+import com.vancoding.todo.ui.theme.LOGO_HEIGHT
+import com.vancoding.todo.ui.theme.splashScreenBackground
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.splashScreenBackground),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            modifier = Modifier
+                .size(LOGO_HEIGHT),
+            painter = painterResource(id = getLogo()),
+            contentDescription = stringResource(id = R.string.todo_logo),
+        )
+    }
+}
+
+@Composable
+fun getLogo(): Int {
+    return if (isSystemInDarkTheme()) {
+        R.drawable.logo_dark
+    } else {
+        R.drawable.logo_light
+    }
+}
+
+@Composable
+@Preview
+fun SplashScreenPreview() {
+    SplashScreen()
+}
