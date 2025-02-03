682e82387d00cee7eab869385832b41f685cce22
Joshlanuevo <lanuevo68@gmail.com>
2025-02-03 12:23:29 +0800

    add red background function

--- Todo/app/src/main/java/com/vancoding/todo/ui/screens/list/ListContent.kt
+++ Todo/app/src/main/java/com/vancoding/todo/ui/screens/list/ListContent.kt
@@ -7,21 +7,31 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import com.vancoding.todo.R
 import com.vancoding.todo.data.models.Priority
 import com.vancoding.todo.data.models.ToDoTask
+import com.vancoding.todo.ui.theme.HighPriorityColor
+import com.vancoding.todo.ui.theme.LARGEST_PADDING
 import com.vancoding.todo.ui.theme.LARGE_PADDING
 import com.vancoding.todo.ui.theme.PRIORITY_INDICATOR_SIZE
 import com.vancoding.todo.ui.theme.TASK_ITEM_ELEVATION
@@ -106,6 +116,24 @@ fun DisplayTasks(
                 navigateToTaskScreen = navigateToTaskScreen,
             )
         }
+    }
+}
+
+@Composable
+fun RedBackground(degrees: Float) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(HighPriorityColor)
+            .padding(horizontal = LARGEST_PADDING),
+        contentAlignment = Alignment.CenterEnd
+    ) {
+        Icon(
+            modifier = Modifier.rotate(degrees = degrees),
+            imageVector = Icons.Filled.Delete,
+            contentDescription = stringResource(id = R.string.delete_icon),
+            tint = Color.White,
+        )
     }
 }
 
