996e952cad3e2616d72d79d0e6d63ef22138b304
Joshlanuevo <lanuevo68@gmail.com>
2025-02-06 16:16:51 +0800

    setup api

--- ContactListApp/app/src/main/java/com/vancoding/contactlistapp/data/remote/api/ApiService.kt
+++ ContactListApp/app/src/main/java/com/vancoding/contactlistapp/data/remote/api/ApiService.kt
@@ -1,5 +1,11 @@
 package com.vancoding.contactlistapp.data.remote.api
 
-interface ApiService {
+import com.vancoding.contactlistapp.data.remote.config.ApiConfig
+import com.vancoding.contactlistapp.data.remote.dto.UserResponse
+import retrofit2.Response
+import retrofit2.http.GET
 
+interface ApiService {
+    @GET(ApiConfig.Endpoints.USERS)
+    suspend fun getContactList(): Response<UserResponse>
 }
---
+++ ContactListApp/app/src/main/java/com/vancoding/contactlistapp/data/remote/dto/UserResponse.kt
@@ -0,0 +1,23 @@
+package com.vancoding.contactlistapp.data.remote.dto
+
+data class UserResponse(
+    val page: Int,
+    val per_page: Int,
+    val total: Int,
+    val total_pages: Int,
+    val data: List<UserData>,
+    val support: Support,
+)
+
+data class UserData(
+    val id: Int,
+    val email: String,
+    val first_name: String,
+    val last_name: String,
+    val avatar: String,
+)
+
+data class Support(
+    val url: String,
+    val text: String,
+)
