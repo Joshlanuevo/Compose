a18c35ffe21a5d037971522132005108b6876d3c
Joshlanuevo <lanuevo68@gmail.com>
2025-02-07 12:39:05 +0800

    implement user repository

--- ContactListApp/app/src/main/java/com/vancoding/contactlistapp/data/remote/ResponseWrapper.kt
+++
@@ -1,7 +0,0 @@
-package com.vancoding.contactlistapp.data.remote
-
-sealed class ResponseWrapper<out T> {
-    data class ResponseSuccess<out T>(val data: T?) : ResponseWrapper<T>()
-    data class ResponseFailure(val message: String) : ResponseWrapper<Nothing>()
-    data class LocalNetworkError(val message: String) : ResponseWrapper<Nothing>()
-}
--- ContactListApp/app/src/main/java/com/vancoding/contactlistapp/ui/viewmodel/UserViewModel.kt
+++ ContactListApp/app/src/main/java/com/vancoding/contactlistapp/ui/viewmodel/UserViewModel.kt
@@ -1,20 +1,16 @@
 package com.vancoding.contactlistapp.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.vancoding.contactlistapp.data.remote.ResponseWrapper
 import com.vancoding.contactlistapp.domain.model.User
-import com.vancoding.contactlistapp.domain.usecase.UserUseCase
 import com.vancoding.contactlistapp.utils.LoadState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UserViewModel @Inject constructor(
-    private val userUseCase: UserUseCase,
+
 ): ViewModel() {
 
     private val _users = MutableStateFlow<LoadState<List<User>>>(LoadState.Default)
