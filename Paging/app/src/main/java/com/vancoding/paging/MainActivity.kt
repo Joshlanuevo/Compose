cc278e14edd81cb131758b0cd69cd3cc231f97cc
Joshlanuevo <lanuevo68@gmail.com>
2025-02-10 16:54:58 +0800

    clean main activity

--- Paging/app/src/main/java/com/vancoding/paging/MainActivity.kt
+++ Paging/app/src/main/java/com/vancoding/paging/MainActivity.kt
@@ -18,30 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PagingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            PagingTheme {}
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PagingTheme {
-        Greeting("Android")
     }
 }
