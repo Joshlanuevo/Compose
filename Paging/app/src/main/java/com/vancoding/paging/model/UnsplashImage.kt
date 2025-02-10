2dea5e028962b420e1e0d15f967e30756e5ffc54
Joshlanuevo <lanuevo68@gmail.com>
2025-02-10 17:18:00 +0800

    add serializable and primary key tag in unsplash image class

--- Paging/app/src/main/java/com/vancoding/paging/model/UnsplashImage.kt
+++ Paging/app/src/main/java/com/vancoding/paging/model/UnsplashImage.kt
@@ -2,10 +2,14 @@ package com.vancoding.paging.model
 
 import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.vancoding.paging.utils.Constants.UNSPLASH_IMAGE_TABLE
+import kotlinx.serialization.Serializable
 
 @Entity(tableName = UNSPLASH_IMAGE_TABLE)
+@Serializable
 data class UnsplashImage(
+    @PrimaryKey(autoGenerate = false)
     val id: String,
     @Embedded
     val urls: Urls,
