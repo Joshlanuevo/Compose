e71654b04d4b6d59184a66557138a6d8a20eb08e
Joshlanuevo <lanuevo68@gmail.com>
2025-02-13 15:31:01 +0800

    add viewmodel parameter

--- Paging/app/src/main/java/com/vancoding/paging/navigation/NavGraph.kt
+++ Paging/app/src/main/java/com/vancoding/paging/navigation/NavGraph.kt
@@ -5,15 +5,22 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.vancoding.paging.screens.home.HomeScreen
+import com.vancoding.paging.screens.home.HomeViewModel
 
 @Composable
-fun SetupNavGraph(navController: NavHostController) {
+fun SetupNavGraph(
+    navController: NavHostController,
+    homeViewModel: HomeViewModel,
+) {
     NavHost(
         navController = navController,
         startDestination = Screen.Home.route
     ) {
         composable(route = Screen.Home.route) {
-            HomeScreen(navController = navController)
+            HomeScreen(
+                navController = navController,
+                homeViewModel = homeViewModel,
+            )
         }
         composable(route = Screen.Search.route) {
 //            SearchScreen(navController = navController)
