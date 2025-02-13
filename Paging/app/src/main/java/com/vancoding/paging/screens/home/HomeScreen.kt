cc561364677b0ac7b8ba67f4431b8bfa759e31ec
Joshlanuevo <lanuevo68@gmail.com>
2025-02-13 15:02:21 +0800

    add home viewmodel

--- Paging/app/src/main/java/com/vancoding/paging/screens/home/HomeScreen.kt
+++ Paging/app/src/main/java/com/vancoding/paging/screens/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.vancoding.paging.navigation.Screen
 import com.vancoding.paging.screens.common.ListContent
 
