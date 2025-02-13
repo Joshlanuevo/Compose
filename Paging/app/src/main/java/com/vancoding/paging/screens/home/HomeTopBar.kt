71c2a4af4b819f9586e0bdf334841b33b1343744
Joshlanuevo <lanuevo68@gmail.com>
2025-02-13 14:41:55 +0800

    setup homescreen

---
+++ Paging/app/src/main/java/com/vancoding/paging/screens/home/HomeTopBar.kt
@@ -0,0 +1,36 @@
+package com.vancoding.paging.screens.home
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeTopBar(
+    onSearchClicked: () -> Unit,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Home",
+                color = MaterialTheme.colorScheme.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colorScheme.topAppBarBackgroundColor,
+        actions = {
+            IconButton(
+                onClick = onSearchClicked,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon",
+                    tint = MaterialTheme.colorScheme.topAppBarContentColor
+                )
+            }
+        }
+    )
+}
