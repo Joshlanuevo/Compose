b4176d2518439782dec56709c7b78fe77312efd7
Joshlanuevo <lanuevo68@gmail.com>
2025-02-14 16:16:57 +0800

    implement search paging source

---
+++ Paging/app/src/main/java/com/vancoding/paging/data/paging/SearchPagingSource.kt
@@ -0,0 +1,41 @@
+package com.vancoding.paging.data.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.vancoding.paging.data.remote.UnsplashApi
+import com.vancoding.paging.model.UnsplashImage
+import com.vancoding.paging.utils.Constants.ITEMS_PER_PAGE
+import javax.inject.Inject
+
+class SearchPagingSource @Inject constructor(
+    private val unsplashApi: UnsplashApi,
+    private val query: String,
+): PagingSource<Int, UnsplashImage>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, UnsplashImage> {
+        val currentPage = params.key ?: 1
+        return try {
+            val response = unsplashApi.searchImages(query = query, per_page = ITEMS_PER_PAGE)
+            val endOfPaginationReached = response.images.isEmpty()
+            if (response.images.isNotEmpty()) {
+                LoadResult.Page(
+                    data = response.images,
+                    prevKey = if (currentPage == 1) null else currentPage - 1,
+                    nextKey = if (endOfPaginationReached) null else currentPage + 1,
+                )
+            } else {
+                LoadResult.Page(
+                    data = emptyList(),
+                    prevKey = null,
+                    nextKey = null,
+                )
+            }
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, UnsplashImage>): Int? {
+        return state.anchorPosition
+    }
+}
