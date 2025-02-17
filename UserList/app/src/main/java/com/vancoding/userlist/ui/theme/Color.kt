d8c716150404e887b4e1d1b42d0c7015a8766235
Joshlanuevo <lanuevo68@gmail.com>
2025-02-17 14:18:57 +0800

    add top app bar colors

--- UserList/app/src/main/java/com/vancoding/userlist/ui/theme/Color.kt
+++ UserList/app/src/main/java/com/vancoding/userlist/ui/theme/Color.kt
@@ -1,5 +1,8 @@
 package com.vancoding.userlist.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Blue80 = Color(0xFFB3CDE0)
@@ -9,3 +12,11 @@ val LightBlue80 = Color(0xFFA7C7E7)
 val Blue40 = Color(0xFF1565C0)
 val BlueGrey40 = Color(0xFF546E7A)
 val LightBlue40 = Color(0xFF039BE5)
+
+val ColorScheme.topAppBarContentColor: Color
+    @Composable
+    get() = if (!isSystemInDarkTheme()) Blue80 else BlueGrey80
+
+val ColorScheme.topAppBarBackgroundColor: Color
+    @Composable
+    get() = if (!isSystemInDarkTheme()) Blue40 else BlueGrey40
