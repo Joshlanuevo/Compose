0af481aa69feaa489dc13f098d20e662f0b2bf02
Joshlanuevo <lanuevo68@gmail.com>
2025-02-17 14:11:09 +0800

    add home top app bar

---
+++ UserList/app/src/main/java/com/vancoding/userlist/ui/screens/home/HomeTopAppBar.kt
@@ -0,0 +1,12 @@
+package com.vancoding.userlist.ui.screens.home
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeTopAppBar(
+    onSearchedClick: () -> Unit,
+) {
+}
