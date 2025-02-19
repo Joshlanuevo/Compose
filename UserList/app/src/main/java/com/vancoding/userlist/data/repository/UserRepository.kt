b401cebacf72172c559ea57f6c1812713df3bcbf
Joshlanuevo <lanuevo68@gmail.com>
2025-02-19 14:49:37 +0800

    add network module

--- UserList/app/src/main/java/com/vancoding/userlist/data/repository/UserRepository.kt
+++ UserList/app/src/main/java/com/vancoding/userlist/data/repository/UserRepository.kt
@@ -1,6 +1,9 @@
 package com.vancoding.userlist.data.repository
 
+import com.vancoding.userlist.data.remote.api.UserApi
 import javax.inject.Inject
 
-class UserRepository @Inject constructor() {
+class UserRepository @Inject constructor(
+    private val api: UserApi,
+) {
 }
---
+++ UserList/app/src/main/java/com/vancoding/userlist/modules/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.vancoding.userlist.modules
+
+import com.vancoding.userlist.data.remote.api.UserApi
+import com.vancoding.userlist.data.repository.UserRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://reqres.in/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserApiService(retrofit: Retrofit): UserApi {
+        return retrofit.create(UserApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(apiService: UserApi): UserRepository {
+        return UserRepository(apiService)
+    }
+}
