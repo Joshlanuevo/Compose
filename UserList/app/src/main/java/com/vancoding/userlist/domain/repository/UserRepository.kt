66740d57143df0bce069875a160901492a070e2e
Joshlanuevo <lanuevo68@gmail.com>
2025-02-19 21:38:18 +0800

    add the userrr repository interface

--- UserList/app/src/main/java/com/vancoding/userlist/domain/repository/UserRepository.kt
+++ UserList/app/src/main/java/com/vancoding/userlist/domain/repository/UserRepository.kt
@@ -1,5 +1,7 @@
 package com.vancoding.userlist.domain.repository
 
+import com.vancoding.userlist.domain.model.UserResponse
+
 interface UserRepository {
-    suspend fun getContactList(page: Int):
+    suspend fun getContactList(page: Int): Result<UserResponse>
 }
