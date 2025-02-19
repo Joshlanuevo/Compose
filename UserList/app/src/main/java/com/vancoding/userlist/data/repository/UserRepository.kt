7581fa101c7c372f59cbcc70e9aeca3ed17da4f1
Joshlanuevo <lanuevo68@gmail.com>
2025-02-19 14:54:57 +0800

    implement repository

--- UserList/app/src/main/java/com/vancoding/userlist/data/repository/UserRepository.kt
+++ UserList/app/src/main/java/com/vancoding/userlist/data/repository/UserRepository.kt
@@ -1,9 +1,15 @@
 package com.vancoding.userlist.data.repository
 
 import com.vancoding.userlist.data.remote.api.UserApi
+import com.vancoding.userlist.model.UserResponse
+import retrofit2.Response
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
     private val api: UserApi,
 ) {
+
+    suspend fun getContactList(page: Int): Response<UserResponse> {
+        return api.getContactList(page)
+    }
 }
