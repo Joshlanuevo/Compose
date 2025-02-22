3d3afc4fe4f1e72ef93cc8dd14b6eae56c4dc7ed
Joshlanuevo <lanuevo68@gmail.com>
2025-02-22 16:54:33 +0800

    implement navigation stack

--- UserList/app/src/main/java/com/vancoding/userlist/ui/navigation/NavGraph.kt
+++ UserList/app/src/main/java/com/vancoding/userlist/ui/navigation/NavGraph.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.vancoding.userlist.ui.screens.destinations.userDetailComposable
+import com.vancoding.userlist.ui.screens.destinations.userListComposable
 import com.vancoding.userlist.ui.screens.home.HomeScreen
 import com.vancoding.userlist.ui.screens.user.UserScreen
+import com.vancoding.userlist.utils.Action
 import com.vancoding.userlist.viewmodel.UserListViewModel
 
 @Composable
@@ -17,14 +20,19 @@ fun NavigationStack(
         navController = navController,
         startDestination = Screen.Home.toString(),
     ) {
-        composable(Screen.Home.route) {
-            HomeScreen(
-                navController = navController,
-                userListViewModel = userListViewModel,
-            )
-        }
-        composable(Screen.User.route) {
-            UserScreen(navController = navController)
-        }
+        userListComposable(
+            navigateToUserDetail = { userId ->
+                navController.navigate(Screen.UserDetail(userId = userId).toString())
+            },
+            userListViewModel = userListViewModel,
+        )
+        userDetailComposable(
+            navigateBack = { action ->
+                if (action == Action.NAVIGATE_BACK) {
+                    navController.popBackStack()
+                }
+            },
+            userListViewModel = userListViewModel,
+        )
     }
 }
