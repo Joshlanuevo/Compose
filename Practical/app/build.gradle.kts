1c95709b710fa5228021facc317fc6c7aa848372
Joshlanuevo <lanuevo68@gmail.com>
2025-03-01 17:04:08 +0800

    oop

--- Practical/app/build.gradle.kts
+++ Practical/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.vancoding.practical"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.vancoding.practical"
--- Practical/app/src/main/java/com/vancoding/practical/MainActivity.kt
+++ Practical/app/src/main/java/com/vancoding/practical/MainActivity.kt
@@ -36,12 +36,52 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+
+    val robot : Robot = Robot("Ivan")
+    robot.walk()
+    robot.speak()
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PracticalTheme {
-        Greeting("Android")
+// Class
+class Robot(val name: String) {
+
+    fun walk() {
+        println("$name is walking")
+    }
+
+    fun speak() {
+        println("Hello, I am $name")
     }
 }
+
+/**
+*  Primary Constructor
+*   - The Primary Constructor define and initialize
+*     properties for a class
+* 
+*   Example:
+*       MyClass(parameter1: Type, parameter2: Type) {}
+* **/
+
+/**
+ *  Visibility Modifiers
+ *  public - visible everywhere
+ *  private - visible only in the same file
+ *  open - visible everywhere and can be overridden
+ *  protected - visible in the same file and subclasses
+ *  internal - visible in the same module
+ * **/
+
+/**
+ *  Abstract Class
+ *      - Abstract classes cannot be instantiated by their own, and are
+ *      typically meant to be a subclassed
+ *      - Abstract Functions: Don't have an implementation in the abstract class
+ *      but must be implemented in subclasses
+ * **/
+
+/**
+ *  Interfaces
+ *      - Interfaces is a blueprint for a set of functions or properties
+ *      that a class must implement
+ * **/
