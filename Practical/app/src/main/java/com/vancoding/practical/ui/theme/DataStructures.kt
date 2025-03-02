9fd9ce83d1cf6f1ff5ea8eaa0f7d957ec05d2761
Joshlanuevo <lanuevo68@gmail.com>
2025-03-02 16:38:35 +0800

    collections

---
+++ Practical/app/src/main/java/com/vancoding/practical/ui/theme/DataStructures.kt
@@ -0,0 +1,23 @@
+package com.vancoding.practical.ui.theme
+
+/**
+ *  Mutable vs Immutable Collections
+ *
+ *  Immutable Collections
+ *      - It means that the collection cannot be changed once it is created
+ *      - It means that you cannot add, remove, or modify elements in the collection
+ *      - It means that it supports read-only operations, functionalities and can not be
+ *          modified its elements
+ *      - Immutable Collections and their corresponding methods are:
+ *          List - listOf() and listOf<T>()
+ *          Set - setOf()
+ *          Map - mapOf()
+ *
+ *  Mutable Collections
+ *      - It means that the collection can be changed once it is created
+ *      - It means that you can add, remove, or modify elements in the collection
+ *      - Mutable Collections and their corresponding methods are:
+ *          List - mutableListOf(), arrayListOf(), and ArrayList
+ *          Set - mutableSetOf(), hashSetOf()
+ *          Map - mutableMapOf(), hashMapOf(), and HashMap
+ * **/
