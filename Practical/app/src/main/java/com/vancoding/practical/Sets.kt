fba05c7efe62971c216b50618d23ebe6a39a82d9
Joshlanuevo <lanuevo68@gmail.com>
2025-03-04 18:47:30 +0800

    immutable sets

---
+++ Practical/app/src/main/java/com/vancoding/practical/Sets.kt
@@ -0,0 +1,15 @@
+package com.vancoding.practical
+
+/**
+ *  Set: Unordered collection of unique elements (No Duplicate Elements)
+ * **/
+
+fun main() {
+
+    // Immutable Sets
+    val immutableSet = setOf("Apple", "Banana", "Orange")
+
+    for (immutableSet in immutableSet) {
+        println(immutableSet)
+    }
+}
